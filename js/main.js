01a79cfd8d30de5ab2d2b57725c420f5701c229b
Gabriel Weber <gabriel@fdd.com.br>
2017-04-04 15:50:55 -0300

    move pieces checking rules

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
-
-$(document).ready(function(){
+var board;
 var chess = new Chess();
 
+onDrop = function(sourcePos, targetPos, piece, newPos, oldPos, orientation){
+    var result = chess.move({from: sourcePos, to: targetPos, promotion: 'q'});
+
+    if(result == null)
+    {
+        return 'snapback';
+    }
+};
+
+init = function(){
     var config = {
-draggable: true,
-dropOffBoard: 'snapback',
-position: 'start'
+        draggable: true,
+        position: 'start',
+        onDrop: onDrop
     };
 
     var board = ChessBoard('board', config);
+};
+
+$(document).ready(function(){
+    init();
 });
